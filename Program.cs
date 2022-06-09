98a79e44330ea5ebd9d1e4cf0e65e09991ee1df6
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-06-09 20:01:37 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Выводит дни недели по номеру
+Console.WriteLine("Введите число от 1 до 7");
+int Number = int.Parse(Console.ReadLine());
+if (Number == 1) {
+    Console.WriteLine("Понедельник");
+}
+else if (Number == 2) {
+    Console.WriteLine("Виторник");
+}
+else if (Number == 2) {
+    Console.WriteLine("Виторник");
+}
+else if (Number == 3) {
+    Console.WriteLine("Среда");
+}
+else if (Number == 4) {
+    Console.WriteLine("Четверг");
+}
+else if (Number == 5) {
+    Console.WriteLine("Пятница");
+}
+else if (Number == 6) {
+    Console.WriteLine("Суббота");
+}
+else if (Number == 7) {
+    Console.WriteLine("Воскресенье");
+}
+else {
+    Console.WriteLine("Неверное число");
+}
